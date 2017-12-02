e95f3d7da339195df6f49a88c3efa50caa506828
Frédéric Fafard <faff2302@usherbrooke.ca>
2017-12-02 16:34:30 -0500

    ced fif protocollwafd

---
+++ Code_Atmel/templateProtocole/apps/Template/astudio/stack/phy/atmegarfr2/inc/header.h
@@ -0,0 +1,28 @@
+/*
+ * header.h
+ *
+ * Created: 2017-12-02 14:55:55
+ *  Author: faff2302
+ */ 
+
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#define IDCode "uWLmpJ9elFpnWre3MwBHzDAUtjWSYoa4O5SpdDtNGr9cneVomBA5B24qZJHA9phhcm7mMKBxizZjtNoiNLIciWRyCoUNfW0c78FM" 
+#define IDCodeLen 100
+
+typedef enum Alerte Alerte;
+enum Alerte
+{
+	ALERTE1 = "ALERTE1",
+	ALERTE2 = "ALERTE2", 
+	ALERTE3 = "ALERTE3",
+	ERROR	= "ERROR"
+};
+
+
+
+
+
+#endif /* HEADER_H_ */
